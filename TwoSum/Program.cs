2c1284c87ebf9f2b07d8177393b8f415ab1372d3
Neit2108 <tienld.22810310329@epu.edu.vn>
2025-07-02 13:54:45 +0700

    1. Two Sum

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -2,7 +2,24 @@
 {
     internal class Program
     {
-
+        public static int[] TwoSumBruteForce(int[] nums, int target)
+        {
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[]
+            {
+                -1,-1
+            };
+        }
 
         public static int[] TwoSum(int[] nums, int target)
         {
@@ -30,6 +47,7 @@
             var nums = new int[] { 2, 7, 11, 15 };
             var target = 9;
             var result = TwoSum(nums, target);
+            // var result = TwoSumBruteForce(nums, target);
             Console.WriteLine(result[0] + " " + result[1]);
         }
     }
