cd02566d00d762744b2628da724c3ea7b8bd9f95
Neit2108 <tienld.22810310329@epu.edu.vn>
2025-07-05 11:47:15 +0700

    Merge Two Sorted List

---
+++ MergeTwoSortedList/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace MergeTwoSortedList;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+
+}
+
+internal class Program
+{
+    public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode newList = new ListNode();
+        ListNode current = newList;
+
+        while (list2 != null && list1 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = list1 ?? list2;
+
+        return newList.next;
+    }
+
+    public static void PrintList(ListNode head)
+    {
+        while (head != null)
+        {
+            Console.Write(head.val);
+            if (head.next != null) Console.Write(" → ");
+            head = head.next;
+        }
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        ListNode merged = MergeTwoLists(list1, list2);
+
+        PrintList(merged);
+    }
+}
