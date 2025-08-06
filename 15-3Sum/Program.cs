beb65a13203976258e290ee92941f19bb81c62c6
Neit2108 <tienld.22810310329@epu.edu.vn>
2025-08-06 08:30:29 +0700

    Leetcode 206 : Reverse Linked List

---
+++ 15-3Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _15_3Sum
+{
+    internal class Program
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+
+            List<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length - 2; i++)// i đến nums.Length - 2 -> xet them vi tri nums.Length - 1 
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                    continue;
+
+                int left = i + 1;
+                int right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+
+                    if (sum == 0)
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    }
+                    else if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 206-ReverseLinkedList/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _206_ReverseLinkedList
+{
+    public class ListNode
+    {
+        public int Value { get; set; }
+        public ListNode Next { get; set; }
+        public ListNode(int value = 0, ListNode next = null)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+
+    internal class Program
+    {
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode previous = null;
+            ListNode current = head;
+
+            while (current != null)
+            {
+                ListNode next = current.Next;
+                current.Next = previous;
+                previous = current;
+                current = next;
+            }
+
+            return previous;
+        }
+
+        static void Main(string[] args)
+        {
+
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+            ListNode reversedHead = ReverseList(head);
+            ListNode current = reversedHead;
+            while (current != null)
+            {
+                Console.Write(current.Value + " ");
+                current = current.Next;
+            }
+        }
+    }
+}
