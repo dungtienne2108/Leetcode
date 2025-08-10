49c98a85a9d43661b5b28d800dccd3b2382a7a63
Neit2108 <tienld.22810310329@epu.edu.vn>
2025-08-10 23:58:46 +0700

    Leetcode 141 : Linked List Cycle

---
+++ 141-LinkedListCycle/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _141_LinkedListCycle
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Program
+    {
+        public static bool HasCycle(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return false;
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next; // chay 1 lan
+                fast = fast.next.next; // chay 2 lan
+
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4))));
+            head.next.next.next = head.next;
+            bool hasCycle = HasCycle(head);
+            Console.WriteLine(hasCycle ? "yes" : "no");
+        }
+    }
+}
