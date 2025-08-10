324f1873f5a6fa95451bfe883be6c6f3397dcc45
Neit2108 <tienld.22810310329@epu.edu.vn>
2025-08-10 23:59:03 +0700

    Leetcode 25 : Reverse Nodes In K Group

---
+++ ReverseNodesInKGroup/Program.cs
@@ -0,0 +1,49 @@
+﻿public class ListNode
+{
+    public int Value { get; set; }
+    public ListNode Next { get; set; }
+    public ListNode(int value = 0, ListNode next = null)
+    {
+        Value = value;
+        Next = next;
+    }
+}
+
+internal class Program
+{
+    public static ListNode ReverseKGroup(ListNode head, int k)
+    {
+        ListNode temp = head;
+        for (int i = 0; i < k; i++)
+        {
+            if (temp == null) return head;
+            temp = temp.Next;
+        }
+
+        ListNode previous = null;
+        ListNode current = head;
+
+        for (int i = 0; i < k; i++)
+        {
+            ListNode next = current.Next;
+            current.Next = previous;
+            previous = current;
+            current = next;
+        }
+
+        head.Next = ReverseKGroup(temp, k);
+
+        return previous;
+    }
+
+    static void Main(string[] args)
+    {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode reversed = ReverseKGroup(head, 3);
+        while (reversed != null)
+        {
+            Console.Write($"{reversed.Value} ->");
+            reversed = reversed.Next;
+        }
+    }
+}
